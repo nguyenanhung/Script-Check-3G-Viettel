3a4565afa5473ed4e475bda955da291df855838b
Nguyen An Hung <dev@nguyenanhung.com>
2016-08-09 15:55:54 +0700

    Update check Ip Index

--- Tests.php
+++ Tests.php
@@ -19,22 +19,18 @@ if ($check_ip_v4 === TRUE) {
     $ip_2_index = $ex_ip[0] . '.' . $ex_ip[1];
     // List IP 3 index
     $ip_3_index = $ex_ip[0] . '.' . $ex_ip[1] . '.' . $ex_ip[2];
-    // Array IP, Convert with MySQL Tools
-    // 
     // Kiểm tra IP trong list
     if (in_array($ip_3_index, $list_ip_range_viettel)) {
         // Có tồn tại IP 3 index
         // Chuyển hướng đến link thích hợp
         $is_3g_viettel = TRUE;
+    } elseif (in_array($ip_2_index, $list_ip_range_viettel)) {
+        // Có tồn tại IP 2 index
+        // Chuyển hướng đến link thích hợp
+        $is_3g_viettel = TRUE;
     } else {
-        if (in_array($ip_2_index, $list_ip_range_viettel)) {
-            // Có tồn tại IP 2 index
-            // Chuyển hướng đến link thích hợp
-            $is_3g_viettel = TRUE;
-        } else {
-            // Ko thỏa mãn cả 2 điều kiện, Return False
-            $is_3g_viettel = FALSE;
-        }
+        // Ko thỏa mãn cả 2 điều kiện, Return False
+        $is_3g_viettel = FALSE;
     }
 } else {
     // Return FALSE
