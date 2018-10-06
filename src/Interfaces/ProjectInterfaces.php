4b255e538084a7759ef742f3d78589bd67dbb3a3
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-06 15:01:26 +0700

    Install Composer

---
+++ src/Interfaces/ProjectInterfaces.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Project Script-Check-3G-Viettel.
+ * Created by PhpStorm.
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/6/18
+ * Time: 14:59
+ */
+
+interface ProjectInterfaces
+{
+    const VERSION      = '2.0';
+    const AUTHOR_NAME  = 'Hung Nguyen';
+    const AUTHOR_EMAIL = 'dev@nguyenanhung.com';
+    const PROJECT_NAME = 'Script check 3G Viettel';
+    const VIETTEL      = 'Viettel Mobile';
+
+    /**
+     * Function getVersion
+     *
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 10/6/18 14:59
+     *
+     * @return mixed
+     */
+    public function getVersion();
+}
---
+++ src/Repository/DataRepository.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Project Script-Check-3G-Viettel.
+ * Created by PhpStorm.
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Date: 10/6/18
+ * Time: 14:59
+ */
+
+class DataRepository
+{
+    const CONFIG_PATH = 'config';
+    const CONFIG_EXT  = '.php';
+
+    /**
+     * Function getVersion
+     *
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 9/21/18 00:20
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return self::VERSION;
+    }
+
+    /**
+     * Function getData
+     *
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 9/19/18 13:51
+     *
+     * @param $configName
+     *
+     * @return array|mixed
+     */
+    public static function getData($configName)
+    {
+        $path = __DIR__ . DIRECTORY_SEPARATOR . self::CONFIG_PATH . DIRECTORY_SEPARATOR . $configName . self::CONFIG_EXT;
+        if (is_file($path) && file_exists($path)) {
+            return require($path);
+        }
+
+        return [];
+    }
+}
